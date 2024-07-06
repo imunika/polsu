0aaa508d381caef28d6da1896bbf6cc277f91d13
Ellie Novoa <ellie.novoa@gmail.com>
2024-07-06 13:07:39 -0600

    added edits

--- app/components/CardWorks.jsx
+++ app/components/CardWorks.jsx
@@ -9,10 +9,10 @@ export default function CardWorks({ item }) {
       className="bg-white border border-slate-100 overflow-hidden 
                   shadow-[rgba(0,_0,_0,_0.1)_0px_25px_50px_-12px]
                   hover:shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]
-                  transition-all duration-1000 h-full">
+                  transition-all duration-1000 h-full rounded-lg">
       <Link className='cursor-pointer' href={val.url}>
         <div className='relative bg-white aspect-[4/3]'>
-          <img className="absolute h-full w-full object-cover p-2 cursor-pointer" src={val.cover_img} alt="" />
+          <img className="absolute h-full w-full object-cover p-2 cursor-pointer rounded-xl" src={val.cover_img} alt="" />
           <div className="absolute right-0 bottom-0 mr-7 mb-7">
             {val.icon}
           </div>
